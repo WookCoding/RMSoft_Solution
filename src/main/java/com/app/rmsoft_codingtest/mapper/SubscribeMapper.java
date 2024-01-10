ab3945b71a5ba0964bcd35bdf8a91fbea10346e4
WookCoding <dnrwhddla@gmail.com>
2024-01-11 02:11:19 +0900

    subscribeMapper + test 완료

--- src/main/java/com/app/rmsoft_codingtest/mapper/SubscribeMapper.java
+++ src/main/java/com/app/rmsoft_codingtest/mapper/SubscribeMapper.java
@@ -6,5 +6,5 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface SubscribeMapper {
 
-    public void insertSubscribe(SubscribeVO subscribeVO);
+    public void insert(SubscribeVO subscribeVO);
 }
--- src/test/java/com/app/rmsoft_codingtest/mapper/SubscribeMapperTests.java
+++ src/test/java/com/app/rmsoft_codingtest/mapper/SubscribeMapperTests.java
@@ -1,6 +1,7 @@
 package com.app.rmsoft_codingtest.mapper;
 
 import com.app.rmsoft_codingtest.domain.vo.MemberVO;
+import com.app.rmsoft_codingtest.domain.vo.SubscribeFormVO;
 import com.app.rmsoft_codingtest.domain.vo.SubscribeVO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -16,8 +17,24 @@ public class SubscribeMapperTests {
     @Autowired
     private SubscribeMapper subscribeMapper;
 
+    @Autowired
+    private SubscribeFormMapper subscribeFormMapper;
+
 //    구독 신청
     @Test
     public void insertSubscribeTest(){
+        LocalDateTime dateTime = LocalDateTime.now();
+        SubscribeFormVO subscribeFormVO = subscribeFormMapper.findById(1L);
+
+        SubscribeVO subscribeVO = SubscribeVO.builder()
+                .subscribeFormId(1L)
+                .subscribeStorageSize("1TB")
+                .subscribeUsePeople(4L)
+                .memberId(1L)
+                .subscribeStartDate(dateTime)
+                .subscribeEndDate(dateTime.plusMonths(subscribeFormVO.getSubscribeFormPeriod()))
+                .build();
+
+        subscribeMapper.insert(subscribeVO);
     }
 }
